be400a2ff2671d5200aa6dd80834f338df86688e
Serdar Yurtseven <serdaryurt7@gmail.com>
2022-02-25 00:00:51 +0300

    Folder Operations
    Folder Operations in C#

---
+++ DosyaIslemleri/Metin_Belgesi_Kayit/Metin_Belgesi_Kayit/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Metin_Belgesi_Kayit
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.Filter = "Text Documents|*.txt";
+            saveFileDialog1.Title = "Save Text Document";
+            saveFileDialog1.ShowDialog();
+            StreamWriter sw = new StreamWriter(saveFileDialog1.FileName);
+            sw.WriteLine(richTextBox1.Text);
+            sw.Close();
+            MessageBox.Show("Kayıt Oluşturuldu.");
+        }
+    }
+}
---
+++ DosyaIslemleri/Pdf/Pdf/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Pdf
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void axAcroPDF1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                axAcroPDF1.LoadFile(ofd.FileName);
+            }
+        }
+    }
+}
